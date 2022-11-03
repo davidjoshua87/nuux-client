738b82581a8842ecec34bbfaac4055ed55b2a4c8
adiysfrmdhn <adi.ramadhan@salt.co.id>
2022-11-03 15:11:58 +0700

    add page subscription

---
+++ pages/subscription/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-row dense>
+        <card />
+        <card />
+        <card />
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'isAuthenticated'
+}
+</script>
+
+<style></style>
