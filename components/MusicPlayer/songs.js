2575c316e5e8457ab555cacf8151458c4fd1ecbe
johsua.shing <joshua.shing@salt.co.id>
2024-08-06 23:27:43 +0700

    fix: #16 added songs and style background

--- components/MusicPlayer/songs.js
+++ components/MusicPlayer/songs.js
@@ -1,75 +1,113 @@
 const songs = [
   {
-    title: 'Di Mana Engkau Berada',
+    noTrack: 11,
+    title: 'Iman dan Pengakuan',
     artist: 'My JDs',
-    seconds: 240,
+    genre: 'Contemporary Christian Acoustic Worship',
+    seconds: 201,
     cover:
-      'https://res.cloudinary.com/dj-project/image/upload/v1722793754/musicxperience/di-mana-engkau-berada-cover.png',
-    src: 'https://res.cloudinary.com/dj-project/video/upload/v1722787038/musicxperience/di-mana-engkau-berada-song.mp3'
+      'https://res.cloudinary.com/dj-project/image/upload/v1722956608/musicxperience/Iman-danPengakuan-cover.png',
+    src: 'https://res.cloudinary.com/dj-project/video/upload/v1722956510/musicxperience/Iman-dan-Pengakuan-song.mp3'
   },
   {
-    title: 'Pujilah TUHAN, Hai Jiwaku!',
+    noTrack: 10,
+    title: 'Jalan dan Kebenaran',
     artist: 'My JDs',
-    seconds: 180,
+    genre: 'Modern Praise Acoustic Worship',
+    seconds: 209,
     cover:
-      'https://res.cloudinary.com/dj-project/image/upload/v1722837406/musicxperience/Pujilah-TUHAN-Hai-Jiwaku%21-cover.png',
-    src: 'https://res.cloudinary.com/dj-project/video/upload/v1722837283/musicxperience/Pujilah-TUHAN-Hai-Jiwaku%21-song.mp3'
+      'https://res.cloudinary.com/dj-project/image/upload/v1722957634/musicxperience/Jalan-dan-Kebenaran-cover.png',
+    src: 'https://res.cloudinary.com/dj-project/video/upload/v1722957683/musicxperience/Jalan-dan-Kebenaran-song.mp3'
   },
   {
-    title: 'Istriku',
+    noTrack: 9,
+    title: 'Cahaya Faith',
     artist: 'My JDs',
-    seconds: 213,
+    genre: 'Acoustic Ballad',
+    seconds: 191,
     cover:
-      'https://res.cloudinary.com/dj-project/image/upload/v1722838861/musicxperience/Istriku-cover.png',
-    src: 'https://res.cloudinary.com/dj-project/video/upload/v1722838776/musicxperience/Istriku-song.mp3'
+      'https://res.cloudinary.com/dj-project/image/upload/v1722841713/musicxperience/Cahaya-Faith-cover.png',
+    src: 'https://res.cloudinary.com/dj-project/video/upload/v1722841555/musicxperience/Cahaya-Faith-song.mp3'
   },
   {
+    noTrack: 8,
     title: 'Terang Kasih-Mu',
     artist: 'My JDs',
+    genre: 'Acoustic Worship Soft Rock Orchestral Pop',
     seconds: 240,
     cover:
       'https://res.cloudinary.com/dj-project/image/upload/v1722839478/musicxperience/Terang-Kasih-Mu-cover.png',
     src: 'https://res.cloudinary.com/dj-project/video/upload/v1722839394/musicxperience/Terang-Kasih-Mu-song.mp3'
   },
   {
+    noTrack: 7,
     title: 'Kekuatanku Hanya Kau',
     artist: 'My JDs',
+    genre: 'Worship Ballad Soft Rock Orchestra',
     seconds: 202,
     cover:
       'https://res.cloudinary.com/dj-project/image/upload/v1722840376/musicxperience/Kekuatanku-Hanya-Kau-cover.png',
     src: 'https://res.cloudinary.com/dj-project/video/upload/v1722840226/musicxperience/Kekuatanku-Hanya-Kau-song.mp3'
   },
   {
+    noTrack: 6,
     title: 'Kesetiaan-Mu',
     artist: 'My JDs',
+    genre: 'Soft Praise & Worship Christian Pop',
     seconds: 181,
     cover:
-      'https://res.cloudinary.com/dj-project/image/upload/v1722840710/musicxperience/Kesetiaan-Mu-cover.png',
+      'https://res.cloudinary.com/dj-project/image/upload/v1722956917/musicxperience/Kesetiaan-Mu-cover.png',
     src: 'https://res.cloudinary.com/dj-project/video/upload/v1722840502/musicxperience/Kesetiaan-Mu-song.mp3'
   },
   {
+    noTrack: 5,
+    title: 'Istriku',
+    artist: 'My JDs',
+    genre: 'Piano Ballad Orchestra',
+    seconds: 213,
+    cover:
+      'https://res.cloudinary.com/dj-project/image/upload/v1722838861/musicxperience/Istriku-cover.png',
+    src: 'https://res.cloudinary.com/dj-project/video/upload/v1722838776/musicxperience/Istriku-song.mp3'
+  },
+  {
+    noTrack: 4,
     title: 'Yesus Kasihku',
     artist: 'My JDs',
+    genre: 'Pop Instrument',
     seconds: 107,
     cover:
       'https://res.cloudinary.com/dj-project/image/upload/v1722840963/musicxperience/Yesus-Kasihku-cover.png',
     src: 'https://res.cloudinary.com/dj-project/video/upload/v1722840998/musicxperience/Yesus-Kasihku-song.mp3'
   },
   {
+    noTrack: 3,
     title: 'Cinta Dari Tuhan',
     artist: 'My JDs',
+    genre: 'Pop Instrument',
     seconds: 175,
     cover:
       'https://res.cloudinary.com/dj-project/image/upload/v1722841386/musicxperience/Cinta-Dari-Tuhan-cover.png',
     src: 'https://res.cloudinary.com/dj-project/video/upload/v1722841425/musicxperience/Cinta-Dari-Tuhan-song.mp3'
   },
   {
-    title: 'Cahaya Faith',
+    noTrack: 2,
+    title: 'Pujilah TUHAN, Hai Jiwaku!',
     artist: 'My JDs',
-    seconds: 191,
+    genre: 'Gospel Worship Christian',
+    seconds: 180,
     cover:
-      'https://res.cloudinary.com/dj-project/image/upload/v1722841713/musicxperience/Cahaya-Faith-cover.png',
-    src: 'https://res.cloudinary.com/dj-project/video/upload/v1722841555/musicxperience/Cahaya-Faith-song.mp3'
+      'https://res.cloudinary.com/dj-project/image/upload/v1722837406/musicxperience/Pujilah-TUHAN-Hai-Jiwaku%21-cover.png',
+    src: 'https://res.cloudinary.com/dj-project/video/upload/v1722837283/musicxperience/Pujilah-TUHAN-Hai-Jiwaku%21-song.mp3'
+  },
+  {
+    noTrack: 1,
+    title: 'Di Mana Engkau Berada',
+    artist: 'My JDs',
+    genre: 'Christian Worship Spiritual',
+    seconds: 240,
+    cover:
+      'https://res.cloudinary.com/dj-project/image/upload/v1722793754/musicxperience/di-mana-engkau-berada-cover.png',
+    src: 'https://res.cloudinary.com/dj-project/video/upload/v1722787038/musicxperience/di-mana-engkau-berada-song.mp3'
   }
 ]
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -34,9 +34,50 @@ export default {
   min-height: 100vh;
   font-family: Lato, sans-serif;
   background: rgba(130,39,159,1);
-  background: linear-gradient(90deg,  rgba(130,39,159,1) 0%, rgba(223,131,241,1) 45%, rgba(83,207,224,1) 100%);
+  background: linear-gradient(90deg,  rgba(130,39,159,1) 0%, rgba(223,131,241,1) 50%, rgba(83,207,224,1) 75%);
   background-size: 400% 400%;
   animation: gradient 15s ease infinite;
   -webkit-font-smoothing: antialiased;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+@keyframes scale-up-center {
+  0% {
+    transform: scale(0.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+@keyframes flip-in-hor-bottom {
+  0% {
+    transform: rotateX(80deg);
+    opacity: 0;
+  }
+  100% {
+    transform: rotateX(0);
+    opacity: 1;
+  }
+}
+
+@keyframes appear-smoothly {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
